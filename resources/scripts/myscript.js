5a78537e445646ccac8cd694a8638d9b19fe2118
SUN4S <56125230+SUN4S@users.noreply.github.com>
2021-07-08 18:09:59 +0300

    Going strong
    -> Added animations when hovering over a project
    -> Added buttons in the About Me panel
    -> Added a right-hand side-pannel, which appears when clicked on a project thumbnail
    -> There's padding under the thumbnail that need to be removed, but I cant locate it

---
+++ resources/scripts/myscript.js
@@ -0,0 +1,9 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openProject() {
+    document.getElementById("mySidepanel").style.width = "500px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeProject() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
