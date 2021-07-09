c8bb0b6722949d3b97ac9ab4c3436447a8abeac3
SUN4S <56125230+SUN4S@users.noreply.github.com>
2021-07-09 12:29:08 +0300

    some JS
    -> Added some JS to change sidepanel contents based on which project was selected
    -> Added some CSS and JS to fix spacing in the sidepanel as well as some spacing

--- resources/scripts/myscript.js
+++ resources/scripts/myscript.js
@@ -1,9 +1,40 @@
-/* Set the width of the sidebar to 250px (show it) */
+/* Set the width of the sidebar to 800px (show it) */
 function openProject() {
-    document.getElementById("mySidepanel").style.width = "500px";
-  }
+    document.getElementById("mySidepanel").style.width = "800px";
+    document.getElementById("mySidepanel").style.opacity = "1";
+}
   
   /* Set the width of the sidebar to 0 (hide it) */
-  function closeProject() {
+function closeProject() {
     document.getElementById("mySidepanel").style.width = "0";
-  }
+    document.getElementById("mySidepanel").style.opacity = "0";
+}
+
+
+function firstProject() {
+    openProject();
+    document.getElementById('projectTitle').innerHTML = "First project";
+    document.getElementById('projectDefinition').innerHTML = "Some info on first project";
+    document.getElementById('projectImage').src = 'resources/img/fernando-hernandez-JdoofvUDUwc-unsplash.png';
+}
+
+function secondProject() {
+    openProject();
+    document.getElementById('projectTitle').innerHTML = "Second project";
+    document.getElementById('projectDefinition').innerHTML = "Some info on Second project";
+    document.getElementById('projectImage').src = "resources/img/maxwell-nelson-taiuG8CPKAQ-unsplash.png";
+}
+
+function thirdProject() {
+    openProject();
+    document.getElementById('projectTitle').innerHTML = "Third project";
+    document.getElementById('projectDefinition').innerHTML = "Some info on Third project";
+    document.getElementById('projectImage').src = "resources/img/pankaj-patel-u2Ru4QBXA5Q-unsplash.png";
+}
+
+function fourthProject() {
+    openProject();
+    document.getElementById('projectTitle').innerHTML = "Fourth project";
+    document.getElementById('projectDefinition').innerHTML = "Some info on Fourth project";
+    document.getElementById('projectImage').src = "resources/img/pankaj-patel-u2Ru4QBXA5Q-unsplash.png";
+}
