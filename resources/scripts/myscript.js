5d5e6deb0ebc5a3515825569852d2becf08d296c
Marius <margliaudelis@gmail.com>
2021-07-10 14:56:00 +0300

    Sidepanel improvement
    --> Added correct spacing to the sidepanel, as well as max-width for mobile compatibility
    --> Added sidepanel buttons that will eventually lead to a live preview of a project as well as a github code
    --> Added sidepanel icons to indicate which coding languages were used to create the selected project

--- resources/scripts/myscript.js
+++ resources/scripts/myscript.js
@@ -14,27 +14,60 @@ function closeProject() {
 function firstProject() {
     openProject();
     document.getElementById('projectTitle').innerHTML = "First project";
-    document.getElementById('projectDefinition').innerHTML = "Some info on first project";
+    document.getElementById('projectDefinition').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit facilis amet ab quo, possimus nobis quos. Delectus fugit iusto reiciendis repellat praesentium enim quidem sequi unde facilis, voluptatum eveniet aperiam adipisci maxime officia assumenda, temporibus atque, repudiandae perspiciatis. Aspernatur fuga laudantium facere natus, ullam reiciendis quo? Quam, doloribus asperiores.";
     document.getElementById('projectImage').src = 'resources/img/fernando-hernandez-JdoofvUDUwc-unsplash.png';
+    document.getElementById("langUsed").innerHTML = 
+                                                    `<li><i class="fab fa-html5"></i></li>
+                                                    <li><i class="fab fa-css3-alt"></i></li>
+                                                    <li><i class="fab fa-js-square"></i></li>
+                                                    <li><i class="fab fa-react"></i></li>
+                                                    <li><i class="fab fa-sass"></i></li>`;
+    document.getElementById("livePreview").addEventListener('click', function() {
+        window.open("https://twitter.com");
+    });
 }
 
 function secondProject() {
     openProject();
     document.getElementById('projectTitle').innerHTML = "Second project";
-    document.getElementById('projectDefinition').innerHTML = "Some info on Second project";
+    document.getElementById('projectDefinition').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit facilis amet ab quo, possimus nobis quos. Delectus fugit iusto reiciendis repellat praesentium enim quidem sequi unde facilis, voluptatum eveniet aperiam adipisci maxime officia assumenda, temporibus atque, repudiandae perspiciatis. Aspernatur fuga laudantium facere natus, ullam reiciendis quo? Quam, doloribus asperiores.";
     document.getElementById('projectImage').src = "resources/img/maxwell-nelson-taiuG8CPKAQ-unsplash.png";
+    document.getElementById("langUsed").innerHTML = 
+                                                    `<li><i class="fab fa-html5"></i></li>
+                                                    <li><i class="fab fa-css3-alt"></i></li>
+                                                    <li><i class="fab fa-js-square"></i></li>
+                                                    <li><i class="fab fa-react"></i></li>
+                                                    <li><i class="fab fa-sass"></i></li>`;
+    document.getElementById("livePreview").addEventListener('click', function() {
+        window.open("https://facebook.com");
+    });
 }
 
 function thirdProject() {
     openProject();
     document.getElementById('projectTitle').innerHTML = "Third project";
-    document.getElementById('projectDefinition').innerHTML = "Some info on Third project";
+    document.getElementById('projectDefinition').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit facilis amet ab quo, possimus nobis quos. Delectus fugit iusto reiciendis repellat praesentium enim quidem sequi unde facilis, voluptatum eveniet aperiam adipisci maxime officia assumenda, temporibus atque, repudiandae perspiciatis. Aspernatur fuga laudantium facere natus, ullam reiciendis quo? Quam, doloribus asperiores.";
     document.getElementById('projectImage').src = "resources/img/pankaj-patel-u2Ru4QBXA5Q-unsplash.png";
+    document.getElementById("langUsed").innerHTML = 
+                                                    `<li><i class="fab fa-html5"></i></li>
+                                                    <li><i class="fab fa-css3-alt"></i></li>
+                                                    <li><i class="fab fa-js-square"></i></li>
+                                                    <li><i class="fab fa-react"></i></li>`;
+    document.getElementById("livePreview").addEventListener('click', function() {
+        window.open("https://reddit.com");
+    });
 }
 
 function fourthProject() {
     openProject();
     document.getElementById('projectTitle').innerHTML = "Fourth project";
-    document.getElementById('projectDefinition').innerHTML = "Some info on Fourth project";
+    document.getElementById('projectDefinition').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit facilis amet ab quo, possimus nobis quos. Delectus fugit iusto reiciendis repellat praesentium enim quidem sequi unde facilis, voluptatum eveniet aperiam adipisci maxime officia assumenda, temporibus atque, repudiandae perspiciatis. Aspernatur fuga laudantium facere natus, ullam reiciendis quo? Quam, doloribus asperiores.";
     document.getElementById('projectImage').src = "resources/img/pankaj-patel-u2Ru4QBXA5Q-unsplash.png";
+    document.getElementById("langUsed").innerHTML = 
+                                                `<li><i class="fab fa-html5"></i></li>
+                                                <li><i class="fab fa-css3-alt"></i></li>
+                                                <li><i class="fab fa-js-square"></i></li>`;
+    document.getElementById("livePreview").addEventListener('click', function() {
+        window.open("https://delfi.lt");
+    });
 }
